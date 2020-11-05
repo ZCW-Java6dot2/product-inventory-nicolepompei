ad72296f450f2fa7da5b5180ba3bcffe6f0d5ad2
pompy <nicole.lynn.pompei@gmail.com>
2020-11-04 21:45:15 -0500

    add snare drums properties

--- src/main/java/models/Cymbals.java
+++ src/main/java/models/Cymbals.java
@@ -1,4 +1,11 @@
 package models;
 
 public class Cymbals {
+    private int id;
+    private String name;
+    private String brand;
+    private int size;
+    private int qty;
+    private float price;
+    private String type;
 }
