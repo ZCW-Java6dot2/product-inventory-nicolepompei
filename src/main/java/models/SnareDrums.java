55c4e120529c7438feaa0fc9d0e70ab7d0877da0
pompy <nicole.lynn.pompei@gmail.com>
2020-11-04 21:41:55 -0500

    add snare drums properties

--- src/main/java/models/SnareDrums.java
+++ src/main/java/models/SnareDrums.java
@@ -1,4 +1,11 @@
 package models;
 
 public class SnareDrums {
+    private int id;
+    private String name;
+    private String brand;
+    private int depth;
+    private int qty;
+    private float price;
+    private String shellType;
 }
