0499a376f0de59d7526760cc8fd373117f8e030a
pompy <nicole.lynn.pompei@gmail.com>
2020-11-05 19:16:01 -0500

    add feature snare drum service

--- src/main/java/services/SnareDrumService.java
+++ src/main/java/services/SnareDrumService.java
@@ -1,4 +1,21 @@
 package services;
 
+import models.SnareDrum;
+
+import java.util.ArrayList;
+
 public class SnareDrumService {
+    private static int nextId = 1;
+
+    private ArrayList<SnareDrum> inventory = new ArrayList<>();
+
+    public SnareDrum create(int depth, String brand, String name, String shellType, int qty, float price){
+        SnareDrum createdSnareDrum = new SnareDrum(depth, brand, name, shellType, qty, price, nextId++);
+
+        inventory.add(createdSnareDrum);
+
+        return createdSnareDrum;
+    }
+
+
 }
--- src/test/java/services/SnareDrumServiceTest.java
+++ src/test/java/services/SnareDrumServiceTest.java
@@ -1,4 +1,43 @@
 package services;
 
+import models.SnareDrum;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SnareDrumServiceTest {
+    @Test
+
+    public void createTest(){
+
+        int expectedDepth = 6;
+        String expectedName = "Patina Brass";
+        String expectedBrand = "Pork Pie";
+        String expectedShellType = "Brass";
+        int expectedQty = 3;
+        float expectedPrice = 329.99f;
+
+        SnareDrumService snareDrumService = new SnareDrumService();
+
+        SnareDrum testSnareDrum = snareDrumService.create(expectedDepth,expectedBrand, expectedName, expectedShellType, expectedQty, expectedPrice);
+
+
+        int actualDepth = testSnareDrum.getDepth();
+        String actualName = testSnareDrum.getName();
+        String actualBrand = testSnareDrum.getBrand();
+        String actualShellType = testSnareDrum.getShellType();
+        int actualQty = testSnareDrum.getQty();
+        float actualPrice = testSnareDrum.getPrice();
+        int actualId = testSnareDrum.getId();
+
+
+        Assert.assertEquals(expectedDepth, actualDepth);
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedBrand, actualBrand);
+        Assert.assertEquals(expectedShellType, actualShellType);
+        Assert.assertEquals(expectedQty, actualQty);
+        Assert.assertEquals(expectedPrice, actualPrice, 0.00000009);
+        Assert.assertEquals(Integer.class.getName(), new Integer(actualId).getClass().getName());
+
+
+    }
 }
